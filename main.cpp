a4029d85b961a34926e38a67bc641d3df87f0ede
rakeshcjadav <rakeshcjadav@gmail.com>
2018-10-26 17:15:55 +0530

    Google test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <gtest/gtest.h>
 
 using namespace std;
 
@@ -64,7 +65,7 @@ void MultiThreading()
     pthread_join(even, nullptr);
 }
 */
-
+/*
 class CPerson
 {
 public:
@@ -195,4 +196,33 @@ int main()
     cout<<"b : "<<b;
    // getchar();
     return 0;
+}
+
+*/
+#include <math.h>
+ 
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
+
+GTEST_TEST(SquareRootTest, PositiveNos) { 
+    ASSERT_EQ(6, squareRoot(36.0));
+    ASSERT_EQ(18.0, squareRoot(324.0));
+    ASSERT_EQ(25.4, squareRoot(645.16));
+    ASSERT_EQ(0, squareRoot(0.0));
+}
+ 
+GTEST_TEST(SquareRootTest, NegativeNos) {
+    ASSERT_EQ(-1.0, squareRoot(-15.0));
+    ASSERT_EQ(-1.0, squareRoot(-0.2));
+}
+ 
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
